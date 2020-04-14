02db993b4dd5823e468e8b8228e9e2821dd5996b
laith <mr.layth_2011@hotmail.com>
2020-04-14 20:18:25 +0300

    add new javascript

---
+++ js/script.js
@@ -0,0 +1,14 @@
+'use strict'
+alert(" Atention This web site not for boyes ");
+
+var age= prompt("what is your age");
+
+if( age >= 18 || age == 0 ){
+    alert("Welcome To the Hackers Wep site");
+}
+else {
+    window.location ="goodby.html";
+    
+}
+
+confirm("Click Ok to Enter");
